6610f96ae28431b4ec34a8ea2c112c3ee80c27f9
dbrittocollin <dbrittocollin@gmail.com>
2024-03-09 16:26:30 +0000

    Implemented ZigZag pattern

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        hourGlassPattern(6);
+        //verticalZigZag(12);
+        hour(3);
     }
 
     public static void ReverseRightPyramid(int number) {
@@ -107,64 +108,66 @@ public class Main {
     }
 
     public static void butterflyPattern(int number) {
-        for(int row=1;row<=number;row++){
-            for (int col=1;col<=row;col++){
+        for (int row = 1; row <= number; row++) {
+            for (int col = 1; col <= row; col++) {
                 System.out.print("* ");
             }
-            for (int space=1;space<=number-row;space++){
+            for (int space = 1; space <= number - row; space++) {
                 System.out.print("  ");
             }
-            for (int space=1;space<=number-row;space++){
+            for (int space = 1; space <= number - row; space++) {
                 System.out.print("  ");
             }
-            for (int col=1;col<=row;col++){
+            for (int col = 1; col <= row; col++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
-        for(int row=1;row<=number;row++){
-            for (int col=number-row;col>=0;col--){
+        for (int row = 1; row <= number; row++) {
+            for (int col = number - row; col >= 0; col--) {
                 System.out.print("* ");
             }
-            for (int space=1;space<=row-1;space++){
+            for (int space = 1; space <= row - 1; space++) {
                 System.out.print("  ");
             }
-            for (int space=1;space<=row-1;space++){
+            for (int space = 1; space <= row - 1; space++) {
                 System.out.print("  ");
             }
-            for (int col=number-row;col>=0;col--){
+            for (int col = number - row; col >= 0; col--) {
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
-    public static void hollowTrianglePattern(int number){
-        for (int row=1;row<=number;row++){
-                for(int spaces=1;spaces<=number-row;spaces++){
-                    System.out.print(" ");
-                }
-                int totalColumns=(2*row)-1;
-                for(int col=1;col<=totalColumns;col++) {
-                    if (col == 1 || row == number || col == (2 * row) - 1) {
-                        System.out.print("*");
-                    } else {
-                        System.out.print(" ");
-                    }
-                    System.out.println();
-                }
-        }
-    }
-    public static void reverseHollowTriangle(int number){
-        for (int row=number;row>=1;row--){
-            int totalSpace=row+1;
-            for(int space=row;space<=number;space++){
+
+    public static void hollowTrianglePattern(int number) {
+        for (int row = 1; row <= number; row++) {
+            for (int spaces = 1; spaces <= number - row; spaces++) {
                 System.out.print(" ");
             }
-            int totalColumns=2*row-1;
-            for (int col=1;col<=totalColumns;col++){
-                if(col==1||col==totalColumns||row==number){
+            int totalColumns = (2 * row) - 1;
+            for (int col = 1; col <= totalColumns; col++) {
+                if (col == 1 || row == number || col == (2 * row) - 1) {
                     System.out.print("*");
-                }else{
+                } else {
+                    System.out.print(" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    public static void reverseHollowTriangle(int number) {
+        for (int row = number; row >= 1; row--) {
+            int totalSpace = row + 1;
+            for (int space = row; space <= number; space++) {
+                System.out.print(" ");
+            }
+            int totalColumns = 2 * row - 1;
+            for (int col = 1; col <= totalColumns; col++) {
+                if (col == 1 || col == totalColumns || row == number) {
+                    System.out.print("*");
+                } else {
                     System.out.print(" ");
                 }
             }
@@ -173,33 +176,35 @@ public class Main {
         }
 
     }
-    public static void KPattern(int number){
-        for(int row=1;row<=number;row++){
-            int totalColumns=(number+1)-row;
-            for(int col=1;col<=totalColumns;col++){
+
+    public static void KPattern(int number) {
+        for (int row = 1; row <= number; row++) {
+            int totalColumns = (number + 1) - row;
+            for (int col = 1; col <= totalColumns; col++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        for(int row=1;row<number;row++){
+        for (int row = 1; row < number; row++) {
 
-            for(int col=1;col<=row+1;col++){
+            for (int col = 1; col <= row + 1; col++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
-    public static void rightPascalTriangle(int number){
-        int increment=1;
-        for(int row=1;row<=(number*2)-1;row++){
-            if(row<=number){
-                for (int col=1;col<=row;col++){
+
+    public static void rightPascalTriangle(int number) {
+        int increment = 1;
+        for (int row = 1; row <= (number * 2) - 1; row++) {
+            if (row <= number) {
+                for (int col = 1; col <= row; col++) {
                     System.out.print("* ");
                 }
-            }else {
+            } else {
 
-                int totalColumns=number-increment;
-                for (int col=1;col<=totalColumns;col++){
+                int totalColumns = number - increment;
+                for (int col = 1; col <= totalColumns; col++) {
                     System.out.print("* ");
                 }
                 increment++;
@@ -208,23 +213,23 @@ public class Main {
         }
     }
 
-    public static void hourGlassPattern(int number){
-        for(int row=1;row<=number;row++){
-            for (int col=1;col<=row;col++){
+    public static void hourGlassPattern(int number) {
+        for (int row = 1; row <= number; row++) {
+            for (int col = 1; col <= row; col++) {
                 System.out.print(" ");
             }
-            for(int starColumn=number-row;starColumn>=0;starColumn--){
+            for (int starColumn = number - row; starColumn >= 0; starColumn--) {
                 System.out.print("* ");
 
 
             }
             System.out.println();
         }
-        for(int row=1;row<=number-1;row++){
-            for (int col=number-row-1;col>=0;col--){
+        for (int row = 1; row <= number - 1; row++) {
+            for (int col = number - row - 1; col >= 0; col--) {
                 System.out.print(" ");
             }
-            for(int starColumn=1;starColumn<=row+1;starColumn++){
+            for (int starColumn = 1; starColumn <= row + 1; starColumn++) {
                 System.out.print("* ");
 
 
@@ -234,5 +239,54 @@ public class Main {
 
     }
 
+    public static void verticalZigZag(int number) {
+        for (int row = 1; row <= number; row++) {
 
+            for (int col = 1; col <= 3; col++) {
+                if (row % col == 0 && col == 2) {
+                    System.out.print("*");
+                } else if (row % 4 == 1 && col == 1) {
+                    System.out.print("*");
+
+                } else if (row % 4 == 3 && col == 3) {
+                    System.out.print("*");
+
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+
+        }
+
+    }
+
+    public static void hour(int number){
+        for(int row=1;row<=number;row++){
+            for(int col=1;col<=row;col++){
+                System.out.print(" ");
+            }
+            for (int starColumn = number - row; starColumn >= 0; starColumn--) {
+                System.out.print("* ");
+
+
+            }
+            System.out.println();
+
+        }
+        for(int row=1;row<=number-1;row++){
+            for (int col=1;col<=number-row;col++){
+                System.out.print(" ");
+            }
+            for(int star=1;star<=row+1;star++){
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+
+    }
 }
+
+
